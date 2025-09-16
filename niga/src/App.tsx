a51099ebd76c79c2e59305fe09b7a3bb06639746
Saso <msaso0725@gmail.com>
2025-09-16 10:01:13 +0200

    chopped

--- niga/src/App.tsx
+++ niga/src/App.tsx
@@ -1,21 +1,19 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import ToggleSwitch from "./components/ToggleSwitch";
 import ExtensionCard, { type ExtensionCardType } from "./components/ExtensionCard";
 
 function App() {
-  const [toggled, setToggled] = useState(false);
+  const [lightMode, setLightMode] = useState(true);
 
-  useEffect(() => {
-    document.body.classList.toggle("dark", toggled);
-  }, [toggled]);
   let Hello : ExtensionCardType = {icon: "🤍", title: "Szia", description: "Sigma, sigma on the wall"}
   return (
-    <>
+    <div className={lightMode ? "wrapper light" : "wrapper dark"}>
       <h1>Hali</h1>
+      <button onClick={() => setLightMode(prev => !prev)}>Klikk ide</button>
       <ExtensionCard {...Hello}/>
       <ExtensionCard icon="🖤" title="Szia" description="Beta beta on teh wall"/>
-    </>
+    </div>
   );
 }
 
-export default App;
+export default App
